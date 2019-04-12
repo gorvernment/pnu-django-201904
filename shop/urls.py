352e2e3f59942c2dbc1ad117ade05e85f07f29c1
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-04-12 16:37:37 +0900

    shop_list 추가_2

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import item_list, item_detail, shop_list
+from .views import item_list, item_detail, shop_list, shop_detail
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', item_list),
     path('<int:pk>/', item_detail),
-    path('shop_list/', shop_list),
+    path('shop/', shop_list),
+    path('shop/<int:pk>/', shop_detail),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,17 @@
-# from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Item, Shop
 
-
-# item_list = ListView.as_view(model=Item)
-
-
+# Create your views here.
 def item_list(request):
-    # DB로부터 모든 Item List를 가져올 예정
-    qs = Item.objects.all()  # QuerySet 타입
-    # 템플릿 파일 위치 : shop/templates/shop/item_list.html
+    qs = Item.objects.all()
     return render(request, 'shop/item_list.html', {
-        'item_list': qs,
+        'item_list' : qs,
     })
 
-
 def item_detail(request, pk):
-    item = Item.objects.get(pk=pk)  # 즉시 DB로부터 Fetch
+    item = Item.objects.get(pk=pk)
     return render(request, 'shop/item_detail.html', {
-        'item': item,
+        'item' : item,
     })
 
 def shop_list(request):
@@ -26,4 +19,11 @@ def shop_list(request):
     # 템플릿 파일 위치 : shop/templates/shop/item_list.html
     return render(request, 'shop/shop_list.html', {
         'shop_list': qs,
+    })
+
+
+def shop_detail(request, pk):
+    shop = Shop.objects.get(pk=pk)
+    return render(request, 'shop/shop_detail.html', {
+        'shop' : shop,
     })
