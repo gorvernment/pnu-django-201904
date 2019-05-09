dc546b404fadc8fdad5063fc897489a62d0f38e7
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-05-09 14:38:32 +0900

    edit form 구현

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-from .views import item_list, item_detail, shop_list, shop_detail
+#from .views import item_list, item_detail, shop_list, shop_detail, shop_new
+from . import views
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', item_list),
-    path('<int:pk>/', item_detail),
-    path('shop/', shop_list),
-    path('shop/<int:pk>/', shop_detail),
+    #path('', item_list),
+    #path('new/', shop_new),
+    #path('<int:pk>/', item_detail),
+    #path('shop/', shop_list),
+    #path('shop/<int:pk>/', shop_detail),
+
+    path('', views.shop_list),
+    path('new/', views.shop_new),
+    path('<int:pk>/edit/', views.shop_edit),
+    path('<int:pk>/', views.shop_detail),
+
+    path('items/', views.item_list),
+    path('items/new/', views.item_new),
+    path('items/<int:pk>/', views.item_detail),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Item, Shop
+from .forms import ShopForm, ItemForm
 
 # Create your views here.
 def item_list(request):
@@ -14,16 +16,68 @@ def item_detail(request, pk):
         'item' : item,
     })
 
-def shop_list(request):
-    qs = Shop.objects.all()  # QuerySet 타입
-    # 템플릿 파일 위치 : shop/templates/shop/item_list.html
-    return render(request, 'shop/shop_list.html', {
-        'shop_list': qs,
+# def shop_list(request):
+#     query = request.GET.get('query', '').strip()    #query = request.GET['query']
+#     qs = Shop.objects.all()  # QuerySet 타입
+#     # print(query)
+
+#     if query:
+#         qs = qs.filter(name__icontains=query)
+
+#     # 템플릿 파일 위치 : shop/templates/shop/item_list.html
+#     return render(request, 'shop/shop_list.html', {
+#         'shop_list': qs,
+#         'query' : query,
+#     })
+
+shop_list = ListView.as_view(model=Shop, paginate_by=10)
+
+# def shop_detail(request, pk):
+#     shop = Shop.objects.get(pk=pk)
+#     return render(request, 'shop/shop_detail.html', {
+#         'shop' : shop,
+#     })
+
+shop_detail = DetailView.as_view(model=Shop)
+
+# def shop_new(request):
+#     if request.method == 'POST':
+#         form = ShopForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/shop/')
+#     else:
+#         form = ShopForm()
+#     return render(request, 'shop/shop_form.html', {
+#         'form' : form,
+#     })
+
+shop_new = CreateView.as_view(model=Shop, form_class=ShopForm,
+                               success_url='/shop/')
+
+def shop_edit(request, pk):
+    shop = Shop.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = ShopForm(request.POST, instance=shop)
+        if form.is_valid():
+            form.save()
+            return redirect('/shop/')
+    else:
+        form = ShopForm(instance=shop)
+    return render(request, 'shop/shop_form.html', {
+        'form' : form,
     })
 
 
-def shop_detail(request, pk):
-    shop = Shop.objects.get(pk=pk)
-    return render(request, 'shop/shop_detail.html', {
-        'shop' : shop,
+def item_new(request):
+    if request.method == 'GET':
+        form = ItemForm()
+    else:
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/shop/items/')
+    return render(request, 'shop/item_form.html', {
+        'form' : form,
     })
